08a492c574038257781136092521e15c01da79b9
Curry5004 <yunjin5004@naver.com>
2022-09-12 23:15:41 +0900

    1406, 1874번 풀이중

---
+++ PS/src/beginner_2_1/NM200/nm005/Main.java
@@ -0,0 +1,28 @@
+package beginner_2_1.NM200.nm005;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 문제
+ * URL : https://www.acmicpc.net/problem/1406
+ *  * */
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+		
+		int totalCount = Integer.parseInt(scan.readLine());
+		int[] refArr = new int[totalCount];
+		
+		for(int i = 0; i<totalCount; i++) {
+			/*
+			 * 우선 받는다.
+			 * */
+			int input = Integer.parseInt(scan.readLine());
+		}
+	}
+	
+}
